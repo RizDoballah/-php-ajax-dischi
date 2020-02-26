72e9c0f9751d0ede6f3ecd9d3518bdbdfc142258
Ghezlene Doballah <ghyzounette@gmail.com>
2020-02-26 08:04:34 +0100

    database

---
+++ database.php
@@ -0,0 +1,65 @@
+<?php
+$database = [
+    [
+      'title' => 'New Jersey',
+      'author' => 'Bon Jovi',
+      'year' => 1988,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+    [
+      'title' => 'Live at Wembley',
+      'author' => 'Queen',
+      'year' => 1992,
+      'poster' => 'https://static.posters.cz/image/750/poster/queen-live-at-wembley-i69296.jpg'
+    ],
+    [
+      'title' => 'One more car, One more driver',
+      'author' => 'Eric Clapton',
+      'year' => 2002,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81lR-Qe4X2L._SX466_.jpg'
+    ],
+    [
+      'title' => "Ten Summoner's Tales",
+      'author' => 'Sting',
+      'year' => 1993,
+      'poster' => 'https://img.discogs.com/KuX_-HtHHUipYElQ07Jjga2D01U=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-472264-1370407935-9119.jpeg.jpg'
+    ],
+    [
+      'title' => 'One more car, One more driver',
+      'author' => 'Eric Clapton',
+      'year' => 2002,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81lR-Qe4X2L._SX466_.jpg'
+    ],
+    [
+      'title' => 'Purple rain',
+      'author' => 'Prince',
+      'year' => 1984,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61SsyhuZJoL._SY679_.jpg'
+    ],
+    [
+      'title' => 'Born in the usa',
+      'author' => 'Bruce sprin',
+      'year' => 1984,
+      'poster' => 'https://m.media-amazon.com/images/M/MV5BNmQ5NGJlOGQtMGMzMy00ZDc0LWEyNmQtYjNiNWEwODA2ZTUxXkEyXkFqcGdeQXVyNDE4OTY5NzI@._V1_.jpg'
+    ],
+    [
+      'title' => 'Diavolo in me',
+      'author' => 'Zucchero',
+      'year' => 1989,
+      'poster' => 'https://www.universalmusic.it/dbcommon/file/news/img1/721.jpg'
+    ],
+    [
+      'title' => "C'est la vie",
+      'author' => 'Cheb Khaled',
+      'year' => 2012,
+      'poster' => 'https://a10.gaanacdn.com/images/albums/58/74858/crop_480x480_74858.jpg'
+    ],
+    [
+      'title' => 'La vie en rose',
+      'author' => 'Edith Piaf',
+      'year' => 1945,
+      'poster' => 'https://www.gtmusic.it/2098/edith-piaf-la-vie-en-rose-cd.jpg'
+    ]
+]
+
+ ?>
--- index.php
+++ index.php
@@ -1,10 +1,45 @@
+<?php include __DIR__ . '/database.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <link rel="stylesheet" href="dist/app.css">
+    <title>php-ajax-dischi</title>
   </head>
   <body>
+    <!-- Header -->
+    <header>
+      <div class="header-container">
+        <img src="img/logo.png" alt="logo">
+      </div>
+    </header>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main>
+      <div class="main-container">
+        <div class="cover">
+          <?php foreach ($database as $cds): ?>
+            <img src="<?php echo $cds['poster']; ?>" alt="poster">
+
+
+          <?php endforeach; ?>
+
+
+        </div>
+
+
+
+      </div>
+
+    </main>
+    <!-- /Main -->
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+    <!-- /Footer -->
 
   </body>
 </html>
